1630c9e81f0f8fad92e655836151982e7cd80386
Orchaldir <orchaldir@gmail.com>
2023-06-08 16:27:19 +0200

    [#1] Add ColorName

---
+++ rpg_tools_core/src/model/color.rs
@@ -0,0 +1,22 @@
+/// A color defined by a name.
+/// See https://en.wikipedia.org/wiki/Web_colors.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum ColorName {
+    Aqua,
+    Black,
+    Blue,
+    Fuchsia,
+    Gray,
+    Green,
+    Lime,
+    Maroon,
+    Navy,
+    Olive,
+    Orange,
+    Purple,
+    Red,
+    Silver,
+    Teal,
+    White,
+    Yellow,
+}
