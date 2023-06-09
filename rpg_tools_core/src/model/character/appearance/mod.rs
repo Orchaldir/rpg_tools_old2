bc734ef61dd459af4d432617e0bdd2af79920b26
Orchaldir <orchaldir@gmail.com>
2023-06-09 10:04:36 +0200

    [#1] Add Appearance.HeadOnly

--- rpg_tools_core/src/model/character/appearance/mod.rs
+++ rpg_tools_core/src/model/character/appearance/mod.rs
@@ -4,25 +4,26 @@ use crate::model::length::Length;
 pub mod skin;
 
 /// How does a [`character`](crate::model::character::Character) look like?
-/// Currently this is limited to humanoids.
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Appearance {
-    height: Length,
-    skin: Skin,
+pub enum Appearance {
+    /// The [`character`](crate::model::character::Character) consists only of a head. E.g. a floating skull
+    HeadOnly { height: Length, skin: Skin },
+    /// The [`character`](crate::model::character::Character) is a humanoid. E.g. a human
+    Humanoid { height: Length, skin: Skin },
 }
 
 impl Appearance {
-    pub fn new(height: Length, skin: Skin) -> Self {
-        Self { height, skin }
+    pub fn head(height: Length, skin: Skin) -> Self {
+        Self::HeadOnly { height, skin }
     }
 
-    pub fn skin(&self) -> &Skin {
-        &self.skin
+    pub fn humanoid(height: Length, skin: Skin) -> Self {
+        Self::Humanoid { height, skin }
     }
 }
 
 impl Default for Appearance {
     fn default() -> Self {
-        Self::new(Length::from_metre(1.0), Skin::default())
+        Self::humanoid(Length::from_metre(1.0), Skin::default())
     }
 }
