b247368d920a35cd48adcc8d819e5d0d7703bebb
Orchaldir <orchaldir@gmail.com>
2023-06-10 22:37:04 +0200

    [#1] Render feet

--- rpg_tools_rendering/src/rendering/body.rs
+++ rpg_tools_rendering/src/rendering/body.rs
@@ -38,15 +38,17 @@ impl BodyRenderer {
         let torso_y = 0.21;
         let arm_y = torso_y + arm_height;
         let leg_y = torso_y + torso_height;
+        let foot_y = leg_y + leg_height;
 
-        let torso_start = aabb.get_point(0.5 - average_width / 2.0, torso_y);
+        let torso_start_x = 0.5 - average_width / 2.0;
+        let torso_start = aabb.get_point(torso_start_x, torso_y);
         let torso_size = aabb.size().scale(average_width, torso_height);
         renderer.render_rectangle(&AABB::new(torso_start, torso_size), &options);
 
         let arm_size = aabb.size().scale(arm_width, arm_height);
         let left_arm_start = aabb.get_point(0.5 + average_width / 2.0, torso_y);
         renderer.render_rectangle(&AABB::new(left_arm_start, arm_size), &options);
-        let right_arm_start = aabb.get_point(0.5 - average_width / 2.0 - arm_width, torso_y);
+        let right_arm_start = aabb.get_point(torso_start_x - arm_width, torso_y);
         renderer.render_rectangle(&AABB::new(right_arm_start, arm_size), &options);
 
         let hand_radius = (height as f32 * hands_factor) as u32;
@@ -57,10 +59,17 @@ impl BodyRenderer {
         renderer.render_circle(&right_hand_center, hand_radius, &options);
 
         let leg_size = aabb.size().scale(leg_width, leg_height);
-        let left_leg_start = aabb.get_point(0.5 + average_width / 2.0 - leg_width, leg_y);
+        let left_leg_start_x = 0.5 + average_width / 2.0 - leg_width;
+        let left_leg_start = aabb.get_point(left_leg_start_x, leg_y);
         renderer.render_rectangle(&AABB::new(left_leg_start, leg_size), &options);
-        let right_leg_start = aabb.get_point(0.5 - average_width / 2.0, leg_y);
+        let right_leg_start = aabb.get_point(torso_start_x, leg_y);
         renderer.render_rectangle(&AABB::new(right_leg_start, leg_size), &options);
+
+        let foot_size = aabb.size().scale(feet_width, feet_height);
+        let left_foot_start = aabb.get_point(left_leg_start_x, foot_y);
+        renderer.render_rectangle(&AABB::new(left_foot_start, foot_size), &options);
+        let right_foot_start = aabb.get_point(torso_start_x - feet_width + leg_width, foot_y);
+        renderer.render_rectangle(&AABB::new(right_foot_start, foot_size), &options);
     }
 
     pub fn calculate_head_aabb(&self, aabb: &AABB) -> AABB {
