5a8b2d8ff8389247ec2b1128086705a2c28c033f
Orchaldir <orchaldir@gmail.com>
2023-06-10 23:08:50 +0200

    [#1] Add render_hourglass()

--- rpg_tools_rendering/src/rendering/body.rs
+++ rpg_tools_rendering/src/rendering/body.rs
@@ -1,7 +1,8 @@
 use crate::math::aabb2d::AABB;
+use crate::math::polygon2d::Polygon2d;
 use crate::renderer::Renderer;
 use crate::rendering::RenderConfig;
-use rpg_tools_core::model::character::appearance::body::{Body, BodyWidth};
+use rpg_tools_core::model::character::appearance::body::{Body, BodyShape, BodyWidth};
 use std::ops::Mul;
 
 /// Renders a [`body`](Body).
@@ -40,7 +41,12 @@ impl BodyRenderer {
         let torso_start_x = 0.5 - torso_width / 2.0;
         let torso_start = aabb.get_point(torso_start_x, torso_y);
         let torso_size = aabb.size().scale(torso_width, torso_height);
-        renderer.render_rectangle(&AABB::new(torso_start, torso_size), &options);
+        let torso_aabb = AABB::new(torso_start, torso_size);
+
+        match body.shape {
+            BodyShape::Hourglass => self.render_hourglass(renderer, config, &torso_aabb, body),
+            _ => renderer.render_rectangle(&torso_aabb, &options),
+        }
 
         let arm_size = aabb.size().scale(arm_width, arm_height);
         let left_arm_start = aabb.get_point(0.5 + torso_width / 2.0, torso_y);
@@ -67,6 +73,49 @@ impl BodyRenderer {
         renderer.render_rectangle(&AABB::new(left_foot_start, foot_size), &options);
         let right_foot_start = aabb.get_point(torso_start_x - feet_width + leg_width, foot_y);
         renderer.render_rectangle(&AABB::new(right_foot_start, foot_size), &options);
+    }
+
+    pub fn render_hourglass(
+        &self,
+        renderer: &mut dyn Renderer,
+        config: &RenderConfig,
+        aabb: &AABB,
+        body: &Body,
+    ) {
+        let options = config.get_options(&body.skin);
+        let upper_height = 0.3;
+        let waist_height = 0.5;
+        let lower_height = 0.75;
+
+        let waits_factor = 0.15;
+
+        let top_left = aabb.get_point(0.0, 0.0);
+        let upper_left = aabb.get_point(0.0, upper_height);
+        let waist_left = aabb.get_point(waits_factor, waist_height);
+        let lower_left = aabb.get_point(0.0, lower_height);
+        let bottom_left = aabb.get_point(0.0, 1.0);
+
+        let top_right = aabb.get_point(1.0, 0.0);
+        let upper_right = aabb.get_point(1.0, upper_height);
+        let waist_right = aabb.get_point(1.0 - waits_factor, waist_height);
+        let lower_right = aabb.get_point(1.0, lower_height);
+        let bottom_right = aabb.get_point(1.0, 1.0);
+
+        let polygon = Polygon2d::new(vec![
+            top_left,
+            upper_left,
+            waist_left,
+            lower_left,
+            bottom_left,
+            bottom_right,
+            lower_right,
+            waist_right,
+            upper_right,
+            top_right,
+        ]);
+        let cut = polygon.cut_corners_n(0.25, 0.25, 3).unwrap();
+
+        renderer.render_polygon(&cut, &options);
     }
 
     pub fn calculate_head_aabb(&self, aabb: &AABB) -> AABB {
