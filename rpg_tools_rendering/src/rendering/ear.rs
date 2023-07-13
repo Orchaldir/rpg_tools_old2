16fcc24ff329ee76fe7675b2357079d1e99e42b8
Orchaldir <orchaldir@gmail.com>
2023-07-13 21:52:44 +0200

    [#20] Render both ears

--- rpg_tools_rendering/src/rendering/ear.rs
+++ rpg_tools_rendering/src/rendering/ear.rs
@@ -1,10 +1,10 @@
 use crate::math::aabb2d::AABB;
+use crate::math::polygon2d::Polygon2d;
 use crate::renderer::{RenderOptions, Renderer};
 use crate::rendering::config::RenderConfig;
 use rpg_tools_core::model::character::appearance::ear::Ears;
 use rpg_tools_core::model::character::appearance::head::Head;
 use rpg_tools_core::model::side::Side;
-use crate::math::polygon2d::Polygon2d;
 
 pub fn render_ears(renderer: &mut dyn Renderer, config: &RenderConfig, aabb: &AABB, head: &Head) {
     let options = config.get_skin_options(&head.skin);
@@ -21,6 +21,16 @@ pub fn render_ears(renderer: &mut dyn Renderer, config: &RenderConfig, aabb: &AA
                 &options,
                 aabb,
                 Side::Left,
+                width_eyes,
+                width_mouth,
+            );
+
+            render_normal_ear(
+                renderer,
+                config,
+                &options,
+                aabb,
+                Side::Right,
                 width_eyes,
                 width_mouth,
             );
@@ -41,11 +51,16 @@ pub fn render_normal_ear(
     let half_eye = head_eye / 2.0 - offset;
     let half_mouth = head_mouth / 2.0 - offset;
     let width = 0.08;
+    let sign = side.get_sign();
+    let top_inner_x = half_eye * sign;
+    let top_outer_x = (half_eye + width) * sign;
+    let bottom_inner_x = half_mouth * sign;
+    let bottom_outer_x = (half_mouth + width) * sign;
 
-    let top_inner = aabb.get_point(0.5 + half_eye, config.head.y_eye);
-    let top_outer = aabb.get_point(0.5 + half_eye + width, config.head.y_eye);
-    let bottom_inner = aabb.get_point(0.5 + half_mouth, config.head.y_mouth);
-    let bottom_outer = aabb.get_point(0.5 + half_mouth + width, config.head.y_mouth);
+    let top_inner = aabb.get_point(0.5 + top_inner_x, config.head.y_eye);
+    let top_outer = aabb.get_point(0.5 + top_outer_x, config.head.y_eye);
+    let bottom_inner = aabb.get_point(0.5 + bottom_inner_x, config.head.y_mouth);
+    let bottom_outer = aabb.get_point(0.5 + bottom_outer_x, config.head.y_mouth);
 
     let polygon = Polygon2d::new(vec![
         top_inner,
