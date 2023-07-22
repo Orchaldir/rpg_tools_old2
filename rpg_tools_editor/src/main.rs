f78b6c1bf2b7799025d78d2226ed3fff60588029
Orchaldir <orchaldir@gmail.com>
2023-07-22 08:40:50 +0200

    [#6] Use rocket

--- rpg_tools_editor/src/main.rs
+++ rpg_tools_editor/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use anyhow::Result;
+use rocket::State;
+use rocket_dyn_templates::{context, Template};
+use rpg_tools_core::model::character::manager::CharacterMgr;
+use std::sync::Mutex;
+
+struct EditorData {
+    data: Mutex<CharacterMgr>,
+}
+
+#[get("/")]
+fn home(data: &State<EditorData>) -> Template {
+    let data = data.data.lock().expect("lock shared data");
+    Template::render(
+        "home",
+        context! {
+            characters: data.get_all().len(),
+        },
+    )
+}
+
+#[rocket::main]
+async fn main() -> Result<()> {
+    if let Err(e) = rocket::build()
+        .manage(EditorData {
+            data: Mutex::new(CharacterMgr::default()),
+        })
+        .mount("/", routes![home])
+        .attach(Template::fairing())
+        .launch()
+        .await
+    {
+        drop(e);
+    };
+
+    Ok(())
 }
