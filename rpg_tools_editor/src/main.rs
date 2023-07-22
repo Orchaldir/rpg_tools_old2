0edef7b513e3cc79a907a9373fa5d6d7230e75c9
Orchaldir <orchaldir@gmail.com>
2023-07-22 10:24:46 +0200

    [#6] Init characters

--- rpg_tools_editor/src/main.rs
+++ rpg_tools_editor/src/main.rs
@@ -4,7 +4,14 @@ extern crate rocket;
 use anyhow::Result;
 use rocket::State;
 use rocket_dyn_templates::{context, Template};
+use rpg_tools_core::model::character::appearance::body::{Body, BodyShape};
+use rpg_tools_core::model::character::appearance::head::Head;
+use rpg_tools_core::model::character::appearance::skin::{Skin, SkinColor};
+use rpg_tools_core::model::character::appearance::Appearance;
 use rpg_tools_core::model::character::manager::CharacterMgr;
+use rpg_tools_core::model::color::Color;
+use rpg_tools_core::model::length::Length;
+use rpg_tools_core::model::width::Width;
 use std::sync::Mutex;
 
 struct EditorData {
@@ -26,7 +33,7 @@ fn home(data: &State<EditorData>) -> Template {
 async fn main() -> Result<()> {
     if let Err(e) = rocket::build()
         .manage(EditorData {
-            data: Mutex::new(CharacterMgr::default()),
+            data: Mutex::new(init()),
         })
         .mount("/", routes![home])
         .attach(Template::fairing())
@@ -37,4 +44,33 @@ async fn main() -> Result<()> {
     };
 
     Ok(())
+}
+
+fn init() -> CharacterMgr {
+    let mut manager = CharacterMgr::default();
+
+    for skin in vec![
+        Skin::Skin(SkinColor::Fair),
+        Skin::Skin(SkinColor::Light),
+        Skin::Skin(SkinColor::Medium),
+        Skin::Skin(SkinColor::Tan),
+        Skin::Skin(SkinColor::Dark),
+        Skin::Skin(SkinColor::VeryDark),
+        Skin::Skin(SkinColor::Exotic(Color::Green)),
+    ] {
+        let id = manager.create();
+        let character = manager.get_mut(id).unwrap();
+
+        character.set_appearance(Appearance::humanoid(
+            Body {
+                shape: BodyShape::Rectangle,
+                width: Width::Average,
+                skin,
+            },
+            Head::default(),
+            Length::from_metre(1.5),
+        ))
+    }
+
+    manager
 }
