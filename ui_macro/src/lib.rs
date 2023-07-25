a9e5959bba1b2679557246855d0f19eabf63a1a1
Orchaldir <orchaldir@gmail.com>
2023-07-25 22:46:37 +0200

    [#31] Handle field

--- ui_macro/src/lib.rs
+++ ui_macro/src/lib.rs
@@ -1,5 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::{Data, DataStruct, Fields};
 
 #[proc_macro_derive(ui)]
 pub fn ui_macro_derive(input: TokenStream) -> TokenStream {
@@ -11,19 +12,34 @@ pub fn ui_macro_derive(input: TokenStream) -> TokenStream {
     impl_ui_macro(&ast)
 }
 
-fn impl_ui_macro(ast: &syn::DeriveInput) -> TokenStream {
-    println!("ast={:?}", ast);
+fn impl_ui_macro(input: &syn::DeriveInput) -> TokenStream {
+    println!("input={:?}", input);
 
-    let name = &ast.ident;
+    let name = &input.ident;
 
     println!("name={:?}", name);
 
-    let gen = quote! {
-        impl UI for #name {
-            fn create_viewer(&self, path: &str) {
-                println!("Create Viewer for {} with path '{}'!", stringify!(#name), path);
+    let gen = match &input.data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(fields),
+            ..
+        }) => {
+            let field_name = fields.named.iter().map(|field| &field.ident);
+            let field_type = fields.named.iter().map(|field| &field.ty);
+
+            quote! {
+                impl UI for #name {
+                    fn create_viewer(&self, path: &str) {
+                        println!("Create Viewer for {} with path '{}'!", stringify!(#name), path);
+                        #(
+                            println!("Add field {} of type {}!", stringify!(#field_name), stringify!(#field_type));
+                        )*
+                    }
+                }
             }
         }
+        _ => panic!("Expected a struct with named fields"),
     };
+
     gen.into()
 }
