3942ba4a8a60f839a65c20ccda0900725abb4c18
Orchaldir <orchaldir@gmail.com>
2023-07-25 23:45:11 +0200

    [#31] Handle field 2

--- rpg_tools_core/examples/viewer_macro.rs
+++ rpg_tools_core/examples/viewer_macro.rs
@@ -5,19 +5,30 @@ use rpg_tools_core::ui::UI;
 use ui_macro::ui;
 
 #[derive(ui)]
-struct Test {
+struct InnerStruct {
     number_a: u32,
     number_b: u32,
     number_c: u32,
 }
 
+#[derive(ui)]
+struct OuterStruct {
+    inner: InnerStruct,
+    number_d: u32,
+    number_e: u32,
+}
+
 fn main() {
     println!("Generate tera code for viewer");
 
-    let viewer = Test {
-        number_a: 0,
-        number_b: 0,
-        number_c: 0,
+    let viewer = OuterStruct {
+        inner: InnerStruct {
+            number_a: 0,
+            number_b: 0,
+            number_c: 0,
+        },
+        number_d: 0,
+        number_e: 0,
     };
 
     viewer.create_viewer("start");
--- ui_macro/src/lib.rs
+++ ui_macro/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{Data, DataStruct, Fields};
+use quote::{quote, ToTokens};
+use syn::__private::TokenStream2;
+use syn::{Data, DataStruct, Field, Fields, Type};
 
 #[proc_macro_derive(ui)]
 pub fn ui_macro_derive(input: TokenStream) -> TokenStream {
@@ -24,16 +25,24 @@ fn impl_ui_macro(input: &syn::DeriveInput) -> TokenStream {
             fields: Fields::Named(fields),
             ..
         }) => {
-            let field_name = fields.named.iter().map(|field| &field.ident);
-            let field_type = fields.named.iter().map(|field| &field.ty);
+            let field_quotes: TokenStream2 = fields.named.iter().map(|field| {
+                let field_name = &field.ident;
+                let field_type = &field.ty;
+                let field_type = quote! { #field_type }.to_string();
+
+                if is_integer(field) {
+                    quote! {  println!("Add integer {}!", stringify!(#field_name)); }
+                }
+                else {
+                    quote! {  println!("Add field {} of type {}!", stringify!(#field_name), stringify!(#field_type)); }
+                }
+            }).collect();
 
             quote! {
                 impl UI for #name {
                     fn create_viewer(&self, path: &str) {
                         println!("Create Viewer for {} with path '{}'!", stringify!(#name), path);
-                        #(
-                            println!("Add field {} of type {}!", stringify!(#field_name), stringify!(#field_type));
-                        )*
+                        #field_quotes
                     }
                 }
             }
@@ -42,4 +51,11 @@ fn impl_ui_macro(input: &syn::DeriveInput) -> TokenStream {
     };
 
     gen.into()
+}
+
+fn is_integer(field: &Field) -> bool {
+    match &field.ty {
+        Type::Path(type_path) if type_path.clone().into_token_stream().to_string() == "u32" => true,
+        _ => false,
+    }
 }
