7d2263f19eb931d9526eb6d2523a3cf80bedb5ff
Orchaldir <orchaldir@gmail.com>
2023-07-26 07:15:18 +0200

    [#31] Cleanup

--- ui_macro/src/lib.rs
+++ ui_macro/src/lib.rs
@@ -5,11 +5,7 @@ use syn::{Data, DataEnum, DataStruct, Field, Fields, FieldsNamed, Ident, Type};
 
 #[proc_macro_derive(ui)]
 pub fn ui_macro_derive(input: TokenStream) -> TokenStream {
-    // Construct a representation of Rust code as a syntax tree
-    // that we can manipulate
     let ast = syn::parse(input).unwrap();
-
-    // Build the trait implementation
     impl_ui_macro(&ast)
 }
 
