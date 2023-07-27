2da37954402c6c6f09b67411a33fcd9755d59ae4
Orchaldir <orchaldir@gmail.com>
2023-07-27 20:46:16 +0200

    [#32] Add editor_macro.rs

---
+++ rpg_tools_core/examples/editor_macro.rs
@@ -0,0 +1,23 @@
+extern crate rpg_tools_core;
+extern crate ui_macro;
+
+pub mod util;
+
+use crate::util::write_each;
+use rpg_tools_core::model::character::appearance::Appearance;
+use rpg_tools_core::ui::editor::EditorVisitor;
+use rpg_tools_core::ui::UI;
+
+fn main() {
+    println!("Generate tera code for editor");
+
+    let mut visitor = EditorVisitor::new("appearance".to_string(), "    ".to_string());
+
+    println!("Start visit");
+
+    Appearance::create_viewer(&mut visitor, "start", "");
+
+    println!("Finished visit");
+
+    write_each("editor.txt", visitor.get_lines()).expect("Couldn't write file!");
+}
