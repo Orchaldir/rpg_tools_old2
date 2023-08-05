73e5ed41c6cead566bca7b50a58a6b3c8feebf3f
Orchaldir <orchaldir@gmail.com>
2023-08-05 19:11:12 +0200

    [#17] Render Stubble

--- rpg_tools_rendering/src/rendering/beard/mod.rs
+++ rpg_tools_rendering/src/rendering/beard/mod.rs
@@ -2,6 +2,7 @@ use crate::math::aabb2d::AABB;
 use crate::math::polygon2d::Polygon2d;
 use crate::renderer::Renderer;
 use crate::rendering::config::RenderConfig;
+use crate::rendering::head::render_head_shape_with_option;
 use rpg_tools_core::model::character::appearance::beard::Beard;
 use rpg_tools_core::model::character::appearance::head::{Head, HeadShape};
 use rpg_tools_core::model::color::Color;
@@ -28,26 +29,21 @@ fn render_stubble(
     color: Color,
 ) {
     let options = config.without_line(color);
+    let line = config.get_line_options(1.0);
     let polygon = get_stubble(config, aabb, head.shape);
 
     renderer.render_polygon(&polygon, &options);
+    render_head_shape_with_option(renderer, config, aabb, line, head.shape);
 }
 
 fn get_stubble(config: &RenderConfig, aabb: &AABB, head_shape: HeadShape) -> Polygon2d {
-    let (top_left, top_right) =
-        aabb.get_mirrored_points(config.head.get_eye_width(head_shape), config.head.y_eye);
-    let (forehead_left, forehead_right) =
-        aabb.get_mirrored_points(config.head.get_mouth_width(head_shape), config.head.y_mouth);
+    let top_width =
+        (config.head.get_eye_width(head_shape) + config.head.get_mouth_width(head_shape)) / 2.0;
+    let top_y = (config.head.y_eye + config.head.y_mouth) / 2.0;
+    let (top_left, top_right) = aabb.get_mirrored_points(top_width, top_y);
     let (bottom_left, bottom_right) =
         aabb.get_mirrored_points(config.head.get_chin_width(head_shape), 1.0);
-    let corners = vec![
-        top_left,
-        forehead_left,
-        bottom_left,
-        bottom_right,
-        forehead_right,
-        top_right,
-    ];
+    let corners = vec![top_left, bottom_left, bottom_right, top_right];
 
     let polygon = Polygon2d::new(corners);
     config.cut_corners(&polygon).unwrap()
