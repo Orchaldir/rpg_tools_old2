6f2c984887954d3829480e38acab7cd275b4f1ec
Orchaldir <orchaldir@gmail.com>
2023-08-06 12:25:00 +0200

    [#17] Render toothbrush

--- rpg_tools_rendering/src/rendering/beard/moustache.rs
+++ rpg_tools_rendering/src/rendering/beard/moustache.rs
@@ -3,11 +3,10 @@ use crate::math::polygon2d::Polygon2d;
 use crate::rendering::config::RenderConfig;
 
 pub fn get_toothbrush(config: &RenderConfig, aabb: &AABB) -> Polygon2d {
-    let height = 0.1;
+    let height = 0.08;
     let width = 0.1;
-    let center_y = config.head.get_moustache_y();
-    let top_y = center_y - height / 2.0;
-    let bottom_y = center_y + height / 2.0;
+    let bottom_y = config.head.y_mouth - 0.01;
+    let top_y = bottom_y - height;
     let (top_left, top_right) = aabb.get_mirrored_points(width, top_y);
     let (bottom_left, bottom_right) = aabb.get_mirrored_points(width, bottom_y);
     let corners = vec![top_left, bottom_left, bottom_right, top_right];
