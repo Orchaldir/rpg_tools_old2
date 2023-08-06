6e79d7a8e28d9544b5cc9d5261ca3f0fa48057e0
Orchaldir <orchaldir@gmail.com>
2023-08-06 22:42:55 +0200

    [#17] extract get_simple()

--- rpg_tools_rendering/src/rendering/beard/moustache.rs
+++ rpg_tools_rendering/src/rendering/beard/moustache.rs
@@ -57,34 +57,31 @@ pub fn get_handlebar(config: &RenderConfig, aabb: &AABB, mouth_width: f32) -> Po
 
 pub fn get_pencil(config: &RenderConfig, aabb: &AABB, mouth_width: f32) -> Polygon2d {
     let height = 0.02;
-    let bottom_y = config.head.y_mouth - height;
-    let top_y = bottom_y - height;
-    let (top_left, top_right) = aabb.get_mirrored_points(mouth_width, top_y);
-    let (bottom_left, bottom_right) = aabb.get_mirrored_points(mouth_width, bottom_y);
-    let corners = vec![top_left, bottom_left, bottom_right, top_right];
-
-    Polygon2d::new(corners)
+    get_simple(config, aabb, height, height, mouth_width, mouth_width)
 }
 
 pub fn get_pyramid(config: &RenderConfig, aabb: &AABB, mouth_width: f32) -> Polygon2d {
     let height = 0.08;
-    let width = 0.08;
-    let bottom_y = config.head.y_mouth - 0.01;
-    let top_y = bottom_y - height;
-    let (top_left, top_right) = aabb.get_mirrored_points(width, top_y);
-    let (bottom_left, bottom_right) = aabb.get_mirrored_points(mouth_width, bottom_y);
-    let corners = vec![top_left, bottom_left, bottom_right, top_right];
-
-    Polygon2d::new(corners)
+    get_simple(config, aabb, height, 0.01, height, mouth_width)
 }
 
 pub fn get_toothbrush(config: &RenderConfig, aabb: &AABB) -> Polygon2d {
     let height = 0.08;
-    let width = 0.08;
-    let bottom_y = config.head.y_mouth - 0.01;
+    get_simple(config, aabb, height, 0.01, height, height)
+}
+
+fn get_simple(
+    config: &RenderConfig,
+    aabb: &AABB,
+    height: f32,
+    offset: f32,
+    top_width: f32,
+    bottom_width: f32,
+) -> Polygon2d {
+    let bottom_y = config.head.y_mouth - offset;
     let top_y = bottom_y - height;
-    let (top_left, top_right) = aabb.get_mirrored_points(width, top_y);
-    let (bottom_left, bottom_right) = aabb.get_mirrored_points(width, bottom_y);
+    let (top_left, top_right) = aabb.get_mirrored_points(top_width, top_y);
+    let (bottom_left, bottom_right) = aabb.get_mirrored_points(bottom_width, bottom_y);
     let corners = vec![top_left, bottom_left, bottom_right, top_right];
 
     Polygon2d::new(corners)
