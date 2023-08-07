425b0a5a6509ec453eb3a0caae323c7da80d09cb
Orchaldir <orchaldir@gmail.com>
2023-08-07 19:06:40 +0200

    [#35] use macro

--- rpg_tools_core/src/model/character/appearance/beard/moustache.rs
+++ rpg_tools_core/src/model/character/appearance/beard/moustache.rs
@@ -1,54 +1,16 @@
 use crate::ui::{UiVisitor, UI};
+use macro_convert::Convert;
 use macro_ui::ui;
 use serde::{Deserialize, Serialize};
-use std::fmt;
-use MoustacheStyle::*;
 
 /// What kind of moustache?
-#[derive(ui, Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Convert, ui, Default, Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum MoustacheStyle {
     FuManchu,
+    #[default]
     Handlebar,
     Pencil,
     Pyramid,
     Toothbrush,
     Walrus,
-}
-
-impl MoustacheStyle {
-    pub fn get_all() -> Vec<MoustacheStyle> {
-        vec![FuManchu, Handlebar, Pencil, Pyramid, Toothbrush, Walrus]
-    }
-}
-
-impl fmt::Display for MoustacheStyle {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl From<&str> for MoustacheStyle {
-    fn from(shape: &str) -> Self {
-        match shape {
-            "FuManchu" => FuManchu,
-            "Pencil" => Pencil,
-            "Pyramid" => Pyramid,
-            "Toothbrush" => Toothbrush,
-            "Walrus" => Walrus,
-            _ => Handlebar,
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_conversion() {
-        for style in MoustacheStyle::get_all() {
-            let string = style.to_string();
-            assert_eq!(style, MoustacheStyle::from(&*string));
-        }
-    }
 }
--- rpg_tools_core/src/model/character/appearance/eye/pupil.rs
+++ rpg_tools_core/src/model/character/appearance/eye/pupil.rs
@@ -1,48 +1,13 @@
 use crate::ui::{UiVisitor, UI};
+use macro_convert::Convert;
 use macro_ui::ui;
 use serde::{Deserialize, Serialize};
-use std::fmt;
-use PupilShape::*;
 
 /// What is the shape of the pupil?
-#[derive(ui, Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Convert, ui, Default, Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum PupilShape {
+    #[default]
     Circle,
     HorizontalSlit,
     VerticalSlit,
-}
-
-impl PupilShape {
-    pub fn get_all() -> Vec<PupilShape> {
-        vec![Circle, HorizontalSlit, VerticalSlit]
-    }
-}
-
-impl fmt::Display for PupilShape {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl From<&str> for PupilShape {
-    fn from(shape: &str) -> Self {
-        match shape {
-            "HorizontalSlit" => HorizontalSlit,
-            "VerticalSlit" => VerticalSlit,
-            _ => Circle,
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_conversion() {
-        for shape in PupilShape::get_all() {
-            let string = shape.to_string();
-            assert_eq!(shape, PupilShape::from(&*string));
-        }
-    }
 }
