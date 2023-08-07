e428eea18ebafbcce7e76e4f0a5536cc7e44b5b0
Orchaldir <orchaldir@gmail.com>
2023-08-07 07:40:59 +0200

    [#17] Improve

--- rpg_tools_rendering/src/rendering/config/example.rs
+++ rpg_tools_rendering/src/rendering/config/example.rs
@@ -28,7 +28,7 @@ pub fn create_config() -> RenderConfig {
     RenderConfig {
         border: 500,
         line_color: WebColor::from_color(Color::Black),
-        line_width: 30,
+        line_width: 15,
         cut_corners_u: 0.25,
         cut_corners_v: 0.25,
         cut_corners_n: 3,
