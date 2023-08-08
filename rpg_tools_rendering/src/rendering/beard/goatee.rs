b8f52140f75548072134b014fdc7646269193ba4
Orchaldir <orchaldir@gmail.com>
2023-08-08 09:49:36 +0200

    [#61] Refactor

--- rpg_tools_rendering/src/rendering/beard/goatee.rs
+++ rpg_tools_rendering/src/rendering/beard/goatee.rs
@@ -4,7 +4,7 @@ use crate::rendering::config::RenderConfig;
 
 pub fn get_goat_patch(config: &RenderConfig, aabb: &AABB, mouth_width: f32) -> Polygon2d {
     let width = mouth_width * 0.8;
-    let mut polygon = from_top_and_bottom(aabb, config.head.y_mouth + 0.01, 1.05, width, width);
+    let mut polygon = from_mouth_to_bottom(config, aabb, 1.05, width, width);
 
     polygon.create_sharp_corner(0);
     polygon.create_sharp_corner(4);
@@ -21,23 +21,33 @@ pub fn get_goatee(config: &RenderConfig, aabb: &AABB, mouth_width: f32) -> Polyg
 
 pub fn get_soul_patch(config: &RenderConfig, aabb: &AABB) -> Polygon2d {
     let height = 0.1;
-    from_top_and_bottom(
-        aabb,
-        config.head.y_mouth + 0.01,
-        config.head.y_mouth + height,
-        height,
-        height,
-    )
+    from_mouth_to_bottom(config, aabb, config.head.y_mouth + height, height, height)
 }
 
 pub fn get_van_dyke(config: &RenderConfig, aabb: &AABB) -> Polygon2d {
     let width = 0.1;
-    let mut polygon = from_top_and_bottom(aabb, config.head.y_mouth + 0.01, 1.05, width, width);
+    let mut polygon = from_mouth_to_bottom(config, aabb, 1.05, width, width);
 
     polygon.create_sharp_corner(0);
     polygon.create_sharp_corner(4);
 
     config.cut_corners(&polygon).unwrap()
+}
+
+fn from_mouth_to_bottom(
+    config: &RenderConfig,
+    aabb: &AABB,
+    bottom_y: f32,
+    top_width: f32,
+    bottom_width: f32,
+) -> Polygon2d {
+    from_top_and_bottom(
+        aabb,
+        config.head.y_mouth + 0.01,
+        bottom_y,
+        top_width,
+        bottom_width,
+    )
 }
 
 fn from_top_and_bottom(
