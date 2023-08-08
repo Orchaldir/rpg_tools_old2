e7dcbbc887f8f85fdbed911eec1db66f519fb4d8
Orchaldir <orchaldir@gmail.com>
2023-08-08 08:57:59 +0200

    [#61] Add GoateeStyle

--- rpg_tools_core/src/model/character/appearance/beard/mod.rs
+++ rpg_tools_core/src/model/character/appearance/beard/mod.rs
@@ -1,9 +1,11 @@
+use crate::model::character::appearance::beard::goatee::GoateeStyle;
 use crate::model::character::appearance::beard::moustache::MoustacheStyle;
 use crate::model::color::Color;
 use crate::ui::{UiVisitor, UI};
 use macro_ui::ui;
 use serde::{Deserialize, Serialize};
 
+pub mod goatee;
 pub mod moustache;
 
 /// How does the beard look like?
@@ -13,4 +15,5 @@ pub enum Beard {
     None,
     Stubble { color: Color },
     Moustache { style: MoustacheStyle, color: Color },
+    Goatee { style: GoateeStyle, color: Color },
 }
