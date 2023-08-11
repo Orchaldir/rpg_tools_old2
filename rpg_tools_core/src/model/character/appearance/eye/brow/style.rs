439afc3ef23a18c3761990671f0e20c7fca6489c
Orchaldir <orchaldir@gmail.com>
2023-08-11 09:28:22 +0200

    [#24] comments

--- rpg_tools_core/src/model/character/appearance/eye/brow/style.rs
+++ rpg_tools_core/src/model/character/appearance/eye/brow/style.rs
@@ -6,8 +6,11 @@ use serde::{Deserialize, Serialize};
 /// What is the style of the eyebrow?
 #[derive(Convert, ui, Default, Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum EyebrowStyle {
+    /// The eyebrows have a constant thickness.
     #[default]
     Even,
+    /// The eyebrows are thicker on the side of the face than in the center.
     Managerial,
+    /// The eyebrows are thicker in the center of the face than on the side.
     Winged,
 }
