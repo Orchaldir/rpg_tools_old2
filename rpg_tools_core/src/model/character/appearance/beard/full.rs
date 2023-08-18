1545793d8ca3dc14f6fe2f45e8048bcd02e554bd
Orchaldir <orchaldir@gmail.com>
2023-08-18 18:39:26 +0200

    [#17] Add full beard

---
+++ rpg_tools_core/src/model/character/appearance/beard/full.rs
@@ -0,0 +1,13 @@
+use crate::ui::{UiVisitor, UI};
+use macro_convert::Convert;
+use macro_ui::ui;
+use serde::{Deserialize, Serialize};
+
+/// What kind of full board?
+#[derive(Convert, ui, Default, Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum FullBeardStyle {
+    Fork,
+    Rectangle,
+    #[default]
+    Triangle,
+}
--- rpg_tools_core/src/model/character/appearance/beard/mod.rs
+++ rpg_tools_core/src/model/character/appearance/beard/mod.rs
@@ -1,3 +1,4 @@
+use crate::model::character::appearance::beard::full::FullBeardStyle;
 use crate::model::character::appearance::beard::goatee::GoateeStyle;
 use crate::model::character::appearance::beard::moustache::MoustacheStyle;
 use crate::model::color::Color;
@@ -5,6 +6,7 @@ use crate::ui::{UiVisitor, UI};
 use macro_ui::ui;
 use serde::{Deserialize, Serialize};
 
+pub mod full;
 pub mod goatee;
 pub mod moustache;
 
@@ -27,6 +29,10 @@ pub enum Beard {
     GoateeAndMoustache {
         moustache: MoustacheStyle,
         goatee: GoateeStyle,
+        color: Color,
+    },
+    FullBeard {
+        style: FullBeardStyle,
         color: Color,
     },
 }
