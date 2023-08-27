60db69b9e50956ad67fd232de97b4fd661012bff
Orchaldir <orchaldir@gmail.com>
2023-08-27 19:49:09 +0200

    [#26] Render more buns

--- rpg_tools_rendering/src/rendering/hair/bun.rs
+++ rpg_tools_rendering/src/rendering/hair/bun.rs
@@ -16,18 +16,28 @@ pub fn render_buns(
     color: Color,
 ) {
     let options = config.get_options(color);
-    let radius = aabb.calculate_from_height(match size {
+    let radius_factor = match size {
         Size::Small => 0.15,
         Size::Medium => 0.2,
         Size::Large => 0.25,
-    });
+    };
+    let radius = aabb.calculate_from_height(radius_factor);
 
     match style {
-        BunStyle::Low => {}
+        BunStyle::Low => {
+            let center = aabb.get_point(0.5, 1.0 - radius_factor);
+            renderer.render_circle(&center, radius, &options);
+        }
         BunStyle::High => {
             let center = aabb.get_point(0.5, 0.0);
             renderer.render_circle(&center, radius, &options);
         }
-        BunStyle::Twin => {}
+        BunStyle::Twin => {
+            let width = config.head.get_top_width(head_shape);
+            let radius = (0.85 * radius as f32) as u32;
+            let (left, right) = aabb.get_mirrored_points(width, 0.1);
+            renderer.render_circle(&left, radius, &options);
+            renderer.render_circle(&right, radius, &options);
+        }
     }
 }
