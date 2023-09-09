11cf6745c9deeec9ffbe74f793571cbfbe279ad5
Orchaldir <orchaldir@gmail.com>
2023-09-09 22:33:10 +0200

    [#71] Fix

--- rpg_tools_rendering/examples/eye_shapes.rs
+++ rpg_tools_rendering/examples/eye_shapes.rs
@@ -20,8 +20,6 @@ pub mod utils;
 fn main() {
     let shape_options = vec![EyeShape::Almond, EyeShape::Ellipse, EyeShape::Circle];
     let faces = HeadShape::get_all();
-    let test = 5;
-    let test3 = 5;
 
     render_2_sets(
         "eye_shapes.svg",
