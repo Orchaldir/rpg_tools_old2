fb4bb8511000e62cd2891e66dbc25c3cd61988c0
Orchaldir <orchaldir@gmail.com>
2023-09-11 22:11:13 +0200

    [#36] MR

--- rpg_tools_rendering/src/rendering/eye/mod.rs
+++ rpg_tools_rendering/src/rendering/eye/mod.rs
@@ -18,13 +18,13 @@ pub fn render_eyes(renderer: &mut dyn Renderer, config: &RenderConfig, aabb: &AA
         Eyes::None => {}
         Eyes::One {
             eye,
-            eyebrows: eyebrow,
+            eyebrows,
         } => {
             let half_height = config.eye.get_half_height(eye.get_shape(), radius);
             let center = aabb.get_point(0.5, config.head.y_eye);
             let eye_aabb = AABB::with_radii(center, radius, half_height);
             render_eye(renderer, config, &eye_aabb, eye);
-            render_eyebrow(renderer, config, eyebrow, &eye_aabb);
+            render_eyebrow(renderer, config, eyebrows, &eye_aabb);
         }
         Eyes::Two {
             eye,
