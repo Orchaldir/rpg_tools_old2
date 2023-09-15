eea952145e2f436d725dc58abd2f39535e5ba781
Orchaldir <orchaldir@gmail.com>
2023-09-15 23:39:37 +0200

    [#41] Render hot pants

--- rpg_tools_rendering/src/rendering/equipment/pants.rs
+++ rpg_tools_rendering/src/rendering/equipment/pants.rs
@@ -1,6 +1,7 @@
 use crate::math::aabb2d::AABB;
 use crate::math::polygon2d::Polygon2d;
 use crate::renderer::Renderer;
+use crate::rendering::config::body::BodyConfig;
 use crate::rendering::config::RenderConfig;
 use rpg_tools_core::model::character::appearance::body::Body;
 use rpg_tools_core::model::equipment::appearance::pants::{Pants, PantsStyle};
@@ -14,19 +15,18 @@ pub fn render_pants(
 ) {
     let options = config.get_options(pants.color);
     let polygon = match pants.style {
-        PantsStyle::HotPants => get_hot_pants(config, aabb, body, pants),
-        _ => get_hot_pants(config, aabb, body, pants),
+        PantsStyle::HotPants => get_hot_pants(&config.body, aabb, body, pants),
+        _ => get_hot_pants(&config.body, aabb, body, pants),
     };
 
     renderer.render_rounded_polygon(&polygon, &options);
 }
 
-fn get_hot_pants(config: &RenderConfig, aabb: &AABB, body: &Body, pants: &Pants) -> Polygon2d {
-    let torso_aabb = config.body.get_torso_aabb(body, aabb);
-    let torso = config.body.get_torso_config(body.shape);
+fn get_hot_pants(config: &BodyConfig, aabb: &AABB, body: &Body, pants: &Pants) -> Polygon2d {
+    let torso_aabb = config.get_torso_aabb(body, aabb);
+    let torso = config.get_torso_config(body.shape);
 
-    let (lower_left, lower_right) =
-        torso_aabb.get_mirrored_points(torso.hip_width, config.body.y_lower);
+    let (lower_left, lower_right) = torso_aabb.get_mirrored_points(torso.hip_width, config.y_lower);
     let (bottom_left, bottom_right) = torso_aabb.get_mirrored_points(torso.hip_width, 1.0);
 
     Polygon2d::new(vec![
