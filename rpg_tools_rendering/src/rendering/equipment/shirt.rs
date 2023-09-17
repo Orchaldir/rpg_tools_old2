c907124b785a01d4be4a7e5e5f00cfbdf632327b
Orchaldir <orchaldir@gmail.com>
2023-09-17 15:06:13 +0200

    [#72] Render neckline

--- rpg_tools_rendering/src/rendering/equipment/shirt.rs
+++ rpg_tools_rendering/src/rendering/equipment/shirt.rs
@@ -18,29 +18,31 @@ pub fn render_shirt(
     let torso_aabb = config.body.get_torso_aabb(body, aabb);
     let torso = config.body.get_torso_config(body.shape);
     let mut builder = create_torso(&torso_aabb, &config.body, torso);
-    add_neckline(config, &torso_aabb, torso, shirt, &mut builder);
+    add_neckline(&torso_aabb, torso, shirt, &mut builder);
     let polygon = builder.build();
     renderer.render_rounded_polygon(&polygon, &options);
 }
 
-fn add_neckline(
-    config: &RenderConfig,
-    aabb: &AABB,
-    torso: &TorsoConfig,
-    shirt: &Shirt,
-    builder: &mut Polygon2dBuilder,
-) {
+fn add_neckline(aabb: &AABB, torso: &TorsoConfig, shirt: &Shirt, builder: &mut Polygon2dBuilder) {
     match shirt.neckline {
-        Neckline::Boat => {}
-        Neckline::Crew => {
-            let width = torso.shoulder_width / 3.0;
-            builder.add_mirrored_points(&aabb, width, 0.0, true);
-            builder.add_mirrored_points(&aabb, width, 0.1, false);
-        }
+        Neckline::Boat => add_round(&aabb, torso, builder, 0.7, 0.05),
+        Neckline::Crew => add_round(&aabb, torso, builder, 0.3, 0.1),
         Neckline::DeepV => add_v(&aabb, torso, builder, 0.4),
-        Neckline::Scoop => {}
+        Neckline::Scoop => add_round(&aabb, torso, builder, 0.5, 0.2),
         Neckline::V => add_v(&aabb, torso, builder, 0.2),
     }
+}
+
+fn add_round(
+    aabb: &&AABB,
+    torso: &TorsoConfig,
+    builder: &mut Polygon2dBuilder,
+    width: f32,
+    depth: f32,
+) {
+    let width = torso.shoulder_width * width;
+    builder.add_mirrored_points(&aabb, width, 0.0, true);
+    builder.add_mirrored_points(&aabb, width, depth, false);
 }
 
 fn add_v(aabb: &&AABB, torso: &TorsoConfig, builder: &mut Polygon2dBuilder, depth: f32) {
