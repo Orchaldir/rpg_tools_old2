f0d6cc7986c2fa9c196a88784f7ee67d3b2bd61a
Orchaldir <orchaldir@gmail.com>
2023-09-23 17:01:49 +0200

    [#42] Render boot shafts

--- rpg_tools_rendering/src/rendering/equipment/footwear.rs
+++ rpg_tools_rendering/src/rendering/equipment/footwear.rs
@@ -1,6 +1,6 @@
 use crate::math::aabb2d::AABB;
 use crate::math::polygon2d::builder::Polygon2dBuilder;
-use crate::renderer::Renderer;
+use crate::renderer::{RenderOptions, Renderer};
 use crate::rendering::body::render_feet;
 use crate::rendering::config::RenderConfig;
 use rpg_tools_core::model::character::appearance::body::Body;
@@ -16,6 +16,12 @@ pub fn render_footwear(
 ) {
     let options = config.get_options(footwear.color);
 
+    match footwear.style {
+        FootwearStyle::Boots => render_shafts(renderer, config, aabb, body, &options, 0.02),
+        FootwearStyle::KneeHighBoots => render_shafts(renderer, config, aabb, body, &options, 0.12),
+        _ => {}
+    }
+
     if is_foot_visible(footwear.style, from_front) {
         render_feet(renderer, config, aabb, body, &options);
     }
@@ -29,6 +35,41 @@ fn is_foot_visible(style: FootwearStyle, from_front: bool) -> bool {
         FootwearStyle::Slippers => from_front,
         _ => true,
     }
+}
+
+fn render_shafts(
+    renderer: &mut dyn Renderer,
+    config: &RenderConfig,
+    aabb: &AABB,
+    body: &Body,
+    options: &RenderOptions,
+    height: f32,
+) {
+    let (left_center, right_center) = config.body.get_feet_center_x(body);
+
+    render_shaft(renderer, config, aabb, body, options, left_center, height);
+    render_shaft(renderer, config, aabb, body, options, right_center, height);
+}
+
+fn render_shaft(
+    renderer: &mut dyn Renderer,
+    config: &RenderConfig,
+    aabb: &AABB,
+    body: &Body,
+    options: &RenderOptions,
+    center_x: f32,
+    height: f32,
+) {
+    let width = config.body.get_leg_width(body);
+    let y_end = config.body.y_foot;
+    let y_start = y_end - height - config.body.get_foot_radius_factor(body);
+    let mut builder = Polygon2dBuilder::new();
+
+    builder.add_horizontal_pair(aabb, width, center_x, y_start, true);
+    builder.add_horizontal_pair(aabb, width, center_x, y_end, true);
+    let polygon = builder.build();
+
+    renderer.render_polygon(&polygon, &options);
 }
 
 fn render_soles(
