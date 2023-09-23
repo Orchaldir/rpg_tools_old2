4142fd369978550d7b7d3aaad1b3445deee85b37
Orchaldir <orchaldir@gmail.com>
2023-09-23 12:07:04 +0200

    [#42] Update examples

--- rpg_tools_rendering/examples/beards.rs
+++ rpg_tools_rendering/examples/beards.rs
@@ -7,17 +7,10 @@ use rpg_tools_core::model::character::appearance::beard::goatee::GoateeStyle::*;
 use rpg_tools_core::model::character::appearance::beard::moustache::MoustacheStyle;
 use rpg_tools_core::model::character::appearance::beard::moustache::MoustacheStyle::*;
 use rpg_tools_core::model::character::appearance::beard::Beard;
-use rpg_tools_core::model::character::appearance::ear::shape::EarShape;
-use rpg_tools_core::model::character::appearance::ear::Ears;
-use rpg_tools_core::model::character::appearance::eye::pupil::PupilShape;
-use rpg_tools_core::model::character::appearance::eye::shape::EyeShape;
-use rpg_tools_core::model::character::appearance::eye::{Eye, Eyes};
 use rpg_tools_core::model::character::appearance::hair::hairline::{Hairline, HairlineStyle};
-use rpg_tools_core::model::character::appearance::hair::short::ShortHair;
 use rpg_tools_core::model::character::appearance::hair::Hair;
 use rpg_tools_core::model::character::appearance::head::{Head, HeadShape};
-use rpg_tools_core::model::character::appearance::mouth::{Mouth, SpecialTeeth, TeethColor};
-use rpg_tools_core::model::character::appearance::skin::{Skin, SkinColor};
+use rpg_tools_core::model::character::appearance::mouth::Mouth;
 use rpg_tools_core::model::character::appearance::Appearance;
 use rpg_tools_core::model::color::Color;
 use rpg_tools_core::model::length::Length;
@@ -85,33 +78,21 @@ fn create_moustache(moustache: MoustacheStyle) -> (String, Beard) {
 fn create_appearance(height: Length, beard: &Beard, face: &HeadShape) -> Appearance {
     Appearance::head(
         Head {
-            ears: Ears::Normal {
-                shape: EarShape::Round,
-                size: Medium,
-            },
-            eyes: Eyes::Two {
-                eye: Eye::Normal {
-                    eye_shape: EyeShape::Ellipse,
-                    pupil_shape: PupilShape::Circle,
-                    pupil_color: Color::Green,
-                    background_color: Color::White,
-                },
-                eyebrows: Default::default(),
-                distance: Medium,
-            },
+            ears: Default::default(),
+            eyes: Default::default(),
             hair: Hair::Short {
-                style: ShortHair::MiddlePart,
+                style: Default::default(),
                 hairline: Hairline::new(HairlineStyle::Round, Medium),
                 color: Color::SaddleBrown,
             },
             mouth: Mouth::Simple {
                 beard: *beard,
                 width: Medium,
-                teeth: SpecialTeeth::None,
-                teeth_color: TeethColor::White,
+                teeth: Default::default(),
+                teeth_color: Default::default(),
             },
             shape: *face,
-            skin: Skin::normal(SkinColor::Light),
+            skin: Default::default(),
         },
         height,
     )
