7c79e3f3a7614361e28a1d3a45f5ae3cc45c9353
Orchaldir <orchaldir@gmail.com>
2023-10-28 09:15:05 +0200

    [#43] Add is_option_u32()

--- macro_ui/src/utils.rs
+++ macro_ui/src/utils.rs
@@ -1,5 +1,5 @@
 use quote::ToTokens;
-use syn::{DataEnum, Field, Fields, Ident, Type};
+use syn::{DataEnum, Field, Fields, GenericArgument, Ident, PathArguments, Type};
 
 pub fn get_field_type(field: &Field) -> Option<Ident> {
     match &field.ty {
@@ -10,6 +10,32 @@ pub fn get_field_type(field: &Field) -> Option<Ident> {
 
 pub fn is_integer(field: &Field) -> bool {
     matches!(&field.ty, Type::Path(type_path) if type_path.clone().into_token_stream().to_string() == "u32")
+}
+
+pub fn is_option(field: &Field) -> bool {
+    matches!(&field.ty, Type::Path(type_path) if type_path.clone().into_token_stream().to_string().starts_with("Option"))
+}
+
+pub fn is_option_u32(field: &Field) -> bool {
+    match &field.ty {
+        Type::Path(type_path) => {
+            if let Some(segment) = type_path.path.segments.first() {
+                return match &segment.arguments {
+                    PathArguments::AngleBracketed(args) => {
+                        if let Some(arg) = args.args.first() {
+                            if let GenericArgument::Type(t) = arg {
+                                return matches!(t, Type::Path(type_path) if type_path.clone().into_token_stream().to_string().to_string() == "u32");
+                            }
+                        }
+                        false
+                    }
+                    _ => false,
+                };
+            }
+            false
+        }
+        _ => false,
+    }
 }
 
 pub fn is_simple_enum(data: &DataEnum) -> bool {
