e218a68ef31d1b9f57ba96c3e3760a045d6be794
Orchaldir <orchaldir@gmail.com>
2023-10-29 16:01:21 +0100

    [#46] Use FrameType

--- rpg_tools_rendering/src/rendering/equipment/eyewear.rs
+++ rpg_tools_rendering/src/rendering/equipment/eyewear.rs
@@ -1,8 +1,11 @@
 use crate::math::aabb2d::AABB;
 use crate::math::point2d::Point2d;
-use crate::renderer::Renderer;
+use crate::renderer::color::WebColor;
+use crate::renderer::{RenderOptions, Renderer};
 use crate::rendering::config::RenderConfig;
-use rpg_tools_core::model::equipment::appearance::eyewear::{Eyewear, LensShape, LensStyle};
+use rpg_tools_core::model::equipment::appearance::eyewear::{
+    Eyewear, FrameType, LensShape, LensStyle,
+};
 
 pub fn render_eyewear(
     renderer: &mut dyn Renderer,
@@ -39,7 +42,14 @@ fn render_lens(
     center: &Point2d,
     radius: u32,
 ) {
-    let options = config.without_line(style.lens_color);
+    let options = match style.frame_type {
+        FrameType::Wire => RenderOptions::new(
+            WebColor::from_color(style.lens_color),
+            WebColor::from_color(style.frame_color),
+            config.line_width,
+        ),
+        _ => config.without_line(style.lens_color),
+    };
     let radius_y = (radius as f32 * 0.8) as u32;
 
     match style.lens_shape {
