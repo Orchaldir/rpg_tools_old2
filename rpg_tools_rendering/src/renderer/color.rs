6086eaaabb66191cedd66b265b558fea6cac4c47
Orchaldir <orchaldir@gmail.com>
2023-11-01 11:59:28 +0100

    [#46] Add WebColor::Transparent

--- rpg_tools_rendering/src/renderer/color.rs
+++ rpg_tools_rendering/src/renderer/color.rs
@@ -6,6 +6,7 @@ use std::fmt::{Display, Formatter};
 pub enum WebColor {
     /// See https://en.wikipedia.org/wiki/Web_colors.
     Name(String),
+    Transparent(String, u8),
     RBG {
         r: u8,
         g: u8,
@@ -69,14 +70,18 @@ impl Display for WebColor {
     /// ```
     ///# use rpg_tools_rendering::renderer::color::WebColor;
     /// assert_eq!(WebColor::Name("test".to_string()).to_string(), "test");
+    /// assert_eq!(WebColor::Transparent("test".to_string(),128).to_string(), "test|128");
     /// assert_eq!(WebColor::from_rgb(255, 128, 0).to_string(), "#ff8000");
     /// assert_eq!(WebColor::from_rgba(255, 128, 0, 64).to_string(), "#ff800040");
     /// ```
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             WebColor::Name(name) => write!(f, "{}", name.clone()),
-            WebColor::RBG { r, g, b } => write!(f, "#{:02x}{:02x}{:02x}", r, *g, *b),
-            WebColor::RBGA { r, g, b, a } => write!(f, "#{:02x}{:02x}{:02x}{:02x}", r, *g, *b, *a),
+            WebColor::Transparent(name, transparency) => {
+                write!(f, "{}|{}", name.clone(), *transparency)
+            }
+            WebColor::RBG { r, g, b } => write!(f, "#{:02x}{:02x}{:02x}", *r, *g, *b),
+            WebColor::RBGA { r, g, b, a } => write!(f, "#{:02x}{:02x}{:02x}{:02x}", *r, *g, *b, *a),
         }
     }
 }
