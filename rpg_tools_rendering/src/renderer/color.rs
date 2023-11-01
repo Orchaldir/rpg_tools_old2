3dce5d2126357dce3533ca4eefe82e17ab4ee0ce
Orchaldir <orchaldir@gmail.com>
2023-11-01 07:22:05 +0100

    [#46] Add RBGA

--- rpg_tools_rendering/src/renderer/color.rs
+++ rpg_tools_rendering/src/renderer/color.rs
@@ -11,6 +11,12 @@ pub enum WebColor {
         g: u8,
         b: u8,
     },
+    RBGA {
+        r: u8,
+        g: u8,
+        b: u8,
+        a: u8,
+    },
 }
 
 impl WebColor {
@@ -23,6 +29,11 @@ impl WebColor {
     pub fn from_rgb(r: u8, g: u8, b: u8) -> WebColor {
         WebColor::RBG { r, g, b }
     }
+
+    /// Returns a new RGBA color.
+    pub fn from_rgba(r: u8, g: u8, b: u8, a: u8) -> WebColor {
+        WebColor::RBGA { r, g, b, a }
+    }
 }
 
 impl Display for WebColor {
@@ -32,11 +43,13 @@ impl Display for WebColor {
     ///# use rpg_tools_rendering::renderer::color::WebColor;
     /// assert_eq!(WebColor::Name("test".to_string()).to_string(), "test");
     /// assert_eq!(WebColor::from_rgb(255, 128, 0).to_string(), "#ff8000");
+    /// assert_eq!(WebColor::from_rgba(255, 128, 0, 64).to_string(), "#ff800040");
     /// ```
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             WebColor::Name(name) => write!(f, "{}", name.clone()),
             WebColor::RBG { r, g, b } => write!(f, "#{:02x}{:02x}{:02x}", r, *g, *b),
+            WebColor::RBGA { r, g, b, a } => write!(f, "#{:02x}{:02x}{:02x}{:02x}", r, *g, *b, *a),
         }
     }
 }
