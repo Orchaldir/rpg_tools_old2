841d2caa7cbb8f89560753beda7378df69982d08
Orchaldir <orchaldir@gmail.com>
2023-11-08 19:00:19 +0100

    Add species/all

--- rpg_tools_editor/src/route/species.rs
+++ rpg_tools_editor/src/route/species.rs
@@ -2,7 +2,7 @@ use crate::EditorData;
 use rocket::State;
 use rocket_dyn_templates::{context, Template};
 
-#[get("/species")]
+#[get("/species/all")]
 pub fn get_all_species(data: &State<EditorData>) -> Template {
     let data = data.data.lock().expect("lock shared data");
 
