4ffae634a84b495e23388e37f5e3e0257c814462
Orchaldir <orchaldir@gmail.com>
2023-11-09 20:04:18 +0100

    Write races

--- rpg_tools_editor/src/route/race.rs
+++ rpg_tools_editor/src/route/race.rs
@@ -1,9 +1,13 @@
+use crate::io::write;
 use crate::EditorData;
 use rocket::form::Form;
 use rocket::State;
 use rocket_dyn_templates::{context, Template};
 use rpg_tools_core::model::race::gender::GenderOption;
 use rpg_tools_core::model::race::{Race, RaceId};
+use std::path::Path;
+
+const FILE: &str = "resources/races.yaml";
 
 #[get("/race/all")]
 pub fn get_all_races(data: &State<EditorData>) -> Template {
@@ -70,14 +74,21 @@ pub fn update_race(
 
     println!("Update race {} with {:?}", id, update);
 
-    data.race_manager
+    let result = data
+        .race_manager
         .get_mut(RaceId::new(id))
         .map(|race| {
             race.set_name(update.name.trim().to_string());
             race.set_gender_option(update.gender_option.into());
             race
         })
-        .map(|race| get_details_template(id, race))
+        .map(|race| get_details_template(id, race));
+
+    if let Err(e) = write(data.race_manager.get_all(), Path::new(FILE)) {
+        println!("Failed to save the races: {}", e);
+    }
+
+    result
 }
 
 fn get_details_template(id: usize, race: &Race) -> Template {
