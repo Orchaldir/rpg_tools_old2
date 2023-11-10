e3dde1e8d20a56755af5063fc9bb38d332f5b0c9
Orchaldir <orchaldir@gmail.com>
2023-11-10 16:55:23 +0100

    More tests

--- rpg_tools_core/src/usecase/edit/race.rs
+++ rpg_tools_core/src/usecase/edit/race.rs
@@ -45,12 +45,38 @@ mod tests {
     }
 
     #[test]
-    fn test_update_name() {
+    fn test_update_name_of_non_existing_race() {
+        let mut data = RpgData::default();
+
+        assert!(update_race_name(&mut data, RaceId::new(0), "Test").is_err());
+    }
+
+    #[test]
+    fn test_update_valid_name() {
+        test_update_name("Test", "Test");
+    }
+
+    #[test]
+    fn test_update_trimmed_name() {
+        test_update_name(" Name ", "Name");
+    }
+
+    fn test_update_name(input: &str, result: &str) {
         let mut data = RpgData::default();
         let id = data.race_manager.create();
 
-        assert!(update_race_name(&mut data, id, "Test").is_ok());
+        assert!(update_race_name(&mut data, id, input).is_ok());
 
-        assert_eq!("Test", data.race_manager.get(id).map(|r| r.name()).unwrap());
+        assert_eq!(result, data.race_manager.get(id).map(|r| r.name()).unwrap());
+    }
+
+    #[test]
+    fn test_update_duplicate_name() {
+        let mut data = RpgData::default();
+        let id0 = data.race_manager.create();
+        let id1 = data.race_manager.create();
+
+        assert!(update_race_name(&mut data, id0, "Test").is_ok());
+        assert!(update_race_name(&mut data, id1, "Test").is_err());
     }
 }
