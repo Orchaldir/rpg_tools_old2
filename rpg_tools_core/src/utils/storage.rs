6684ff00c38cf48d967aebee853b7ef2f926fcae
Orchaldir <orchaldir@gmail.com>
2023-11-12 10:58:14 +0100

    [#105] Add trait Id

--- rpg_tools_core/src/utils/storage.rs
+++ rpg_tools_core/src/utils/storage.rs
@@ -1,21 +1,33 @@
-pub trait Entry {
+use std::marker::PhantomData;
+
+pub trait Id: Copy {
     fn new(id: usize) -> Self;
 
     fn id(&self) -> usize;
 }
 
-#[derive(Default, Debug)]
-pub struct Storage<T: Entry> {
-    entries: Vec<T>,
+pub trait Entry<I: Id> {
+    fn new(id: I) -> Self;
+
+    fn id(&self) -> I;
 }
 
-impl<T: Entry> Storage<T> {
+#[derive(Default, Debug)]
+pub struct Storage<I: Id, T: Entry<I>> {
+    entries: Vec<T>,
+    phantom: PhantomData<I>,
+}
+
+impl<I: Id, T: Entry<I>> Storage<I, T> {
     pub fn new(entries: Vec<T>) -> Self {
-        Self { entries }
+        Self {
+            entries,
+            phantom: PhantomData,
+        }
     }
 
-    pub fn create(&mut self) -> usize {
-        let id = self.entries.len();
+    pub fn create(&mut self) -> I {
+        let id = Id::new(self.entries.len());
         self.entries.push(T::new(id));
         id
     }
@@ -24,11 +36,11 @@ impl<T: Entry> Storage<T> {
         &self.entries
     }
 
-    pub fn get(&self, id: usize) -> Option<&T> {
-        self.entries.get(id)
+    pub fn get(&self, id: I) -> Option<&T> {
+        self.entries.get(id.id())
     }
 
-    pub fn get_mut(&mut self, id: usize) -> Option<&mut T> {
-        self.entries.get_mut(id)
+    pub fn get_mut(&mut self, id: I) -> Option<&mut T> {
+        self.entries.get_mut(id.id())
     }
 }
