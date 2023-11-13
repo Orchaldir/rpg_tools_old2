27c7ec594f6eebbe4ccc5fc7bbf799f9f5a66a38
Orchaldir <orchaldir@gmail.com>
2023-11-13 21:25:55 +0100

    [#105] More tests

--- rpg_tools_core/src/usecase/delete/race.rs
+++ rpg_tools_core/src/usecase/delete/race.rs
@@ -25,3 +25,41 @@ pub fn delete_race(data: &mut RpgData, id: RaceId) -> DeleteResult {
         Some(_) => DeleteResult::Ok,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use DeleteResult::*;
+
+    #[test]
+    fn test_not_found() {
+        let mut data = RpgData::default();
+
+        assert_eq!(NotFound, delete_race(&mut data, RaceId::default()));
+    }
+
+    #[test]
+    fn test_success() {
+        let mut data = RpgData::default();
+        let id = data.race_manager.create();
+
+        assert_eq!(Ok, delete_race(&mut data, id));
+    }
+
+    #[test]
+    fn test_blocked_by_character() {
+        let mut data = RpgData::default();
+        let race_id = data.race_manager.create();
+        let character_id = data.character_manager.create();
+        data.character_manager
+            .get_mut(character_id)
+            .map(|character| character.set_race(race_id));
+
+        assert_eq!(
+            Blocked {
+                characters: vec![character_id]
+            },
+            delete_race(&mut data, race_id)
+        );
+    }
+}
