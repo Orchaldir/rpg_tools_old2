f48f2f098ab9d1e7315f3165aecd1d690eb98cf7
Orchaldir <orchaldir@gmail.com>
2023-11-17 21:44:24 +0100

    [#112] Improve RelationStorage

--- rpg_tools_core/src/utils/relation.rs
+++ rpg_tools_core/src/utils/relation.rs
@@ -1,35 +1,56 @@
 use crate::utils::storage::Id;
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
 #[derive(Debug)]
-pub struct RelationStorage<I: Id, T> {
+pub struct RelationStorage<I: Id, T: Clone> {
     relations: HashMap<I, HashMap<I, T>>,
 }
 
-impl<I: Id, T> RelationStorage<I, T> {
+impl<I: Id, T: Clone> RelationStorage<I, T> {
     pub fn new(relations: HashMap<I, HashMap<I, T>>) -> Self {
         Self { relations }
     }
 
+    /// Does it contain a specific element?.
+    pub fn contains(&self, id: I) -> bool {
+        self.relations.contains_key(&id)
+    }
+
     /// Gets all relations for a specific element.
-    pub fn get(&self, id: I) -> Option<&HashMap<I, T>> {
+    pub fn get_all_of(&self, id: I) -> Option<&HashMap<I, T>> {
         self.relations.get(&id)
     }
 
     /// Gets the relation between 2 elements.
-    pub fn get_between(&self, from: I, to: I) -> Option<&T> {
+    pub fn get(&self, from: I, to: I) -> Option<&T> {
         self.relations.get(&from).and_then(|map| map.get(&to))
     }
 
     /// Adds a relation between 2 elements.
-    pub fn add_between(&mut self, from: I, to: I, relation: T) {
-        self.relations.entry(from)
-            .or_default()
-            .insert(to, relation);
+    pub fn add(&mut self, from: I, to: I, relation: T) {
+        self.add_one_direction(from, to, relation.clone());
+        self.add_one_direction(to, from, relation);
+    }
+
+    fn add_one_direction(&mut self, from: I, to: I, relation: T) {
+        self.relations.entry(from).or_default().insert(to, relation);
+    }
+
+    /// Deletes the relation between 2 elements.
+    pub fn delete(&mut self, from: I, to: I) {
+        self.delete_one_direction(from, to);
+        self.delete_one_direction(to, from);
+    }
+
+    fn delete_one_direction(&mut self, from: I, to: I) {
+        if let Entry::Occupied(mut e) = self.relations.entry(from) {
+            e.get_mut().remove(&to);
+        }
     }
 }
 
-impl<I: Id, T> Default for RelationStorage<I, T> {
+impl<I: Id, T: Clone> Default for RelationStorage<I, T> {
     fn default() -> Self {
         RelationStorage::new(HashMap::new())
     }
