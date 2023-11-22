ff3b3b69d9398da33dcc51c92cb5cb16f0c30532
Orchaldir <orchaldir@gmail.com>
2023-11-22 21:56:50 +0100

    [#112] Add test

--- rpg_tools_core/src/usecase/delete/character.rs
+++ rpg_tools_core/src/usecase/delete/character.rs
@@ -28,7 +28,13 @@ pub fn delete_character(data: &mut RpgData, id: CharacterId) -> DeleteResult {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::model::character::relation::relationship::Relationship::Friend;
     use DeleteResult::*;
+
+    const RESULT: DeleteResult = Blocked(BlockingReason {
+        characters: vec![],
+        relations: 1,
+    });
 
     #[test]
     fn test_not_found() {
@@ -46,5 +52,16 @@ mod tests {
         let id = data.character_manager.create();
 
         assert_eq!(Ok, delete_character(&mut data, id));
+    }
+
+    #[test]
+    fn test_blocked_by_relationship() {
+        let mut data = RpgData::default();
+        let id0 = data.character_manager.create();
+        let id1 = data.character_manager.create();
+        data.relations.relationships.add(id0, id1, Friend);
+
+        assert_eq!(RESULT, delete_character(&mut data, id0));
+        assert_eq!(RESULT, delete_character(&mut data, id1));
     }
 }
